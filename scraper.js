37194fb5b1540f220c85156932389caccc17d742
AMAN <nottherealaman@gmail.com>
2023-01-31 18:00:02 +0530

    first commit

---
+++ scraper.js
@@ -0,0 +1,23 @@
+const cheerio = require('cheerio')
+const axios = require('axios')
+
+const url ='https://www.amazon.in/Apple-MacBook-Chip-13-inch-256GB/dp/B08N5T6CZ6/ref=sr_1_1_sspa?keywords=macbook&qid=1675003152&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1'
+
+const product ={ name:'' , price:'' , link:''}
+
+
+const scrape = async ()=>{
+    const {data} = await axios.get(url);
+    //console.log(data)
+
+    const $ = cheerio.load(data)
+
+    const item = $('div#dp-container')
+
+    product.name = $(item).find('span.a-offscreen').text()
+
+    console.log(product.name)
+
+}
+scrape()
+
